d18bc29eb4564b95fb5f9ba6335a05a1745263b7
novoneel-iudx <novoneel.chakraborty@datakaveri.org>
2025-05-09 15:22:41 +0530

    [FIX]: Modified to output region column name

--- metrics/relevance_completeness.py
+++ metrics/relevance_completeness.py
@@ -14,10 +14,11 @@ def check_coverage_region(df):
         region column is not null, "No region column found" if there is no region column,
         or "Null values present" if all values in the region column are null.
     """
+    # TODO: Add dict of region columns to check
     region_col = [col for col in df.columns if 'district' or 'state' or 'city' or 'region' or 'subdistrict' in col.lower()]
+    region_column = None if not region_col else region_col[0]
     if not region_col:
-        return {"region_coverage": "No region column found"}
-    region_col = region_col[0]
-    missing_percentage = df[region_col].isnull().mean() * 100
-    return {"region_coverage": missing_percentage}
+        return {"region_coverage": "No region column found", "region_column": None}
+    missing_percentage = df[region_col[0]].isnull().mean() * 100
+    return {"region_coverage": missing_percentage, "region_column": region_column}
 
