c882dc2dc9ec7052768fc375edb0717031eb48c7
novoneel-iudx <novoneel.chakraborty@datakaveri.org>
2025-06-05 16:12:44 +0530

    [DOCS]: Added docstring

--- report/json_writer.py
+++ report/json_writer.py
@@ -2,6 +2,25 @@ import os
 import json
 # from report.aggregate import generate_readiness_report
 def write_report_outputs(report, out_path, dataset_name, additional_report=None):
+    """
+    Write the report outputs to a JSON file.
+
+    Parameters
+    ----------
+    report : dict
+        The main report data to be written to the JSON file.
+    out_path : str
+        The directory path where the JSON file will be saved.
+    dataset_name : str
+        The name of the dataset, used for naming the JSON file.
+    additional_report : dict, optional
+        Additional data to be included in the JSON file, by default None.
+
+    Returns
+    -------
+    None
+    """
+
     os.makedirs(out_path, exist_ok=True)
     filename = f"{dataset_name}_raw_readiness_report.json"
     json_path = os.path.join(out_path, filename)
