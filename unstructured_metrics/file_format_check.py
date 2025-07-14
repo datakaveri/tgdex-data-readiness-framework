8715cea1517bc866090d6257d420455613217b43
novoneel-iudx <novoneel.chakraborty@datakaveri.org>
2025-07-14 16:28:59 +0530

    [FEATURE]: Updated acceptable list of file formats and updated function return

--- unstructured_metrics/file_format_check.py
+++ unstructured_metrics/file_format_check.py
@@ -16,6 +16,9 @@ def check_file_format(directory):
         "Valid" if all the files in the directory are in a standard format,
         otherwise "Invalid".
     """
-    valid_formats = ['.jpg', '.png', '.wav', '.mp3', '.txt', '.csv', '.json', '.pdf', '.docx', '.xlsx', '.pptx', '.html', '.xml','.dicom']
+    valid_formats = ['.xlsx', '.xls', '.pdf', '.mp3', '.jpg', '.png', '.tiff', '.tif', '.txt', '.md', '.dcm']
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    return {"file_format": "Valid" if all(any(f.endswith(fmt) for fmt in valid_formats) for f in files) else "Invalid"}
+    non_valid_format_count = sum(1 for f in files if not any(f.endswith(fmt) for fmt in valid_formats))
+    non_valid_format_percentage = (non_valid_format_count / len(files)) * 100
+    return {"deviant_count": non_valid_format_count, "deviant_percentage": non_valid_format_percentage}
+
