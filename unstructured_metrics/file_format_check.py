ec70c67e66f7b2ed3c320084c48cd6a19dc87fa0
novoneel-iudx <novoneel.chakraborty@datakaveri.org>
2025-07-31 20:36:32 +0530

    [FIX]: Removed annotation files from format consideration

--- unstructured_metrics/file_format_check.py
+++ unstructured_metrics/file_format_check.py
@@ -12,13 +12,21 @@ def check_file_format(directory):
     Returns
     -------
     dict
-        A dictionary with a single key "file_format" which has a value of
-        "Valid" if all the files in the directory are in a standard format,
-        otherwise "Invalid".
+        A dictionary with the following keys:
+        - "valid_format_count": The number of files in the directory that are in a standard format.
+        - "invalid_format_count": The number of files in the directory that are not in a standard format.
+        - "valid_format_percentage": The percentage of files in the directory that are in a standard format.
+        - "invalid_format_percentage": The percentage of files in the directory that are not in a standard format.
     """
     valid_formats = ['.xlsx', '.xls', '.pdf', '.mp3', '.jpg', '.png', '.tiff', '.tif', '.txt', '.md', '.dcm']
-    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    label_keywords = ["label", "annotation", "groundtruth", "tag", "mask", "segmentation", "bbox", "yolo", "class", "target", "output"]
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and not any(keyword in f.lower() for keyword in label_keywords)]
     valid_format_count = sum(1 for f in files if any(f.endswith(fmt) for fmt in valid_formats))
-    valid_format_percentage = (valid_format_count / len(files)) * 100
-    return {"valid_format_count": valid_format_count, "valid_format_percentage": valid_format_percentage}
+    invalid_format_count = len(files) - valid_format_count
+    valid_format_percentage = (valid_format_count / len(files)) * 100 if files else 0.0
+    invalid_format_percentage = 100 - valid_format_percentage
+    return {"valid_format_count": valid_format_count, 
+            "invalid_format_count": invalid_format_count,
+            "valid_format_percentage": valid_format_percentage,
+            "invalid_format_percentage": invalid_format_percentage}
 
